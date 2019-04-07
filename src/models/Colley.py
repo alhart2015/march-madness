da7cdaa539d64d7e50572f16479b461afa702b71
alhart2015 <alhart@davidson.edu>
2019-04-06 22:36:29 -0400

    Skeleton for Massey and Colley

--- src/models/Colley.py
+++ src/models/Colley.py
@@ -6,4 +6,32 @@ todo: inherit from Model?
 todo: port with type hints
 '''
 class Colley:
-    pass
+    
+    def __init__(self):
+        '''
+        todo: this. what fields does it need?
+        '''
+        pass
+
+    def rank(self) -> List[Team]:
+        '''
+        Given a matrix, create a power ranking of the teams
+        '''
+        pass
+
+    def predict_bracket(self) -> Bracket:
+        '''
+        Given a ranking of the teams, and the draw for the bracket, predict who wins and stuff
+        '''
+        pass
+
+    @staticmethod
+    def from_file(filename: str) -> Colley:
+        '''
+        todo: docs
+        todo: weighting param? does that apply for Colley?
+
+        parse teams and games from file
+        create matrix from teams and games
+        '''
+        pass
